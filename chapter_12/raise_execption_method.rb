45a43598348712bb7998cea844bf2ce003257f61
Scott Jensen <scott.jensen@shopify.com>
2018-03-07 16:06:17 -0500

    add chapter 12 & raise_execption_method.rb

---
+++ chapter_12/raise_execption_method.rb
@@ -0,0 +1,23 @@
+class SmallOven
+    attr_accessor :contents
+
+    def turn_on
+        puts "turning the oven on."
+        @state = "On"
+    end
+
+    def turn_off
+        puts "turning the oven off"
+        @state = "Off"
+    end
+
+    def bake
+        unless state == "On"
+            raise "your will need to turn the oven on!"
+        end
+        if @contents == nil
+            raise "There's nothing in the oven"
+        end
+        "golden brown #{contents}"
+    end
+end
